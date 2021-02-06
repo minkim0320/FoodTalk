13df300449140bec8568316410a2e04344eaa34e
Erio Hoti <eriohoti@Erios-MacBook-Pro.local>
2021-02-06 07:51:52 -0500

    login and registration system that works with the database, and has encrypted passwords

--- forms.py
+++ forms.py
@@ -8,19 +8,44 @@ Created on Sat Feb  6 00:17:51 2021
 
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=20)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    username = StringField('Username', 
+                           validators= [DataRequired(), 
+                                        Length(min=3, max=20,message="Username must be between 3 and 20 chars")])
+    email = StringField('Email', 
+                        validators=[DataRequired(), 
+                                    Email(message="Invalid email!")])
+    password = PasswordField('Password', 
+                             validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', 
+                                     validators=[DataRequired(), 
+                                                 EqualTo('password',message="Passwords must match!")])
     submit = SubmitField("Sign up")
     
    
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=20)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField("Login")                                                                                  
+    email = StringField('Email', 
+                        validators=[DataRequired(), 
+                                    Email(message="Are you sure this email exists?")])
+    password = PasswordField('Password', 
+                             validators=[DataRequired()])
+    submit = SubmitField("Login")  
+    business = BooleanField("Are you signing in as a business?")
+
+
+class BusinessForm(FlaskForm):
+    email = StringField('Email', 
+                        validators=[DataRequired(), 
+                                    Email(message="Invalid email!")])
+    password = PasswordField('Password', 
+                             validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', 
+                                     validators=[DataRequired(), 
+                                                 EqualTo('password',message="Passwords must match!")])
+    businessname = StringField('Business',
+                               validators = [Length(min=3, max=20, message="Input your business name")])
+    submit = SubmitField("Register")
+                                                                                    
