6f9369b513b193dc5df29e607e9da9da4aa3ae96
Coding0116 <sianabinkong@gmail.com>
2021-02-06 00:45:24 -0500

    added firebase

--- app.py
+++ app.py
@@ -7,7 +7,22 @@ Created on Fri Feb  5 21:20:53 2021
 """
 
 from flask import Flask, render_template,url_for
+import pyrebase
 app = Flask(__name__)
+
+config = {
+    "apiKey": "AIzaSyCFgKFUtm-kmXEZEoyecrbyxCTtFnLe1ik",
+    "authDomain": "foodtalk-f05ed.firebaseapp.com",
+    "databaseURL": "https://foodtalk-f05ed-default-rtdb.firebaseio.com",
+    "projectId": "foodtalk-f05ed",
+    "storageBucket": "foodtalk-f05ed.appspot.com",
+    "messagingSenderId": "281572446405",
+    "appId": "1:281572446405:web:6e30739ee8a7fc119120ac",
+    "measurementId": "G-CGV0VJ25FT"
+}
+
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
 
 @app.route('/index')
 @app.route('/home')
